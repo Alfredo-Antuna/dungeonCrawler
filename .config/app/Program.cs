c3f45f24a35d0d0025509b7ccd53601f79df20e9
Alfredo Antuna <Alfredo.Antuna@lntinfotech.com>
2021-09-30 16:46:50 -0500

    one

---
+++ .config/app/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using lib;
+namespace app
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var db = new Database();
+
+            bool run = true;
+            while(run)
+            {
+                Scenario.Explore(db);
+                
+            }
+
+        }
+    }
+}
---
+++ .config/lib/Map.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic; 
+
+namespace lib
+{
+    public  class Location
+    {
+       public int Id {get; set;}
+       public string Name {get; set;}
+       public string Description {get; set;}
+       public int Size  {get; set;}
+
+       public string Type {get; set;}
+
+    }
+}
